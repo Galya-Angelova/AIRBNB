3b14fbec99eb572d7157c2acadf3c4db77f57a3a
Kaloian Pavlov <kaloian_pavlov@mail.bg>
2018-05-07 21:12:30 +0300

    Sending emails when making reservation added

--- src/main/java/com/airbnb/controller/reservation/ReservationController.java
+++ src/main/java/com/airbnb/controller/reservation/ReservationController.java
@@ -2,6 +2,7 @@ package com.airbnb.controller.reservation;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.airbnb.exceptions.InvalidPlaceException;
+import com.airbnb.model.email.MailSender;
 import com.airbnb.model.place.PlaceDAO;
 import com.airbnb.model.place.PlaceDTO;
 import com.airbnb.model.reservation.Reservation;
@@ -70,8 +72,14 @@ public class ReservationController {
 		}
 		PlaceDTO place= (PlaceDTO)session.getAttribute("place");
 		reservationDAO.makeReservation(new Reservation(0,start,end,place.getId(),user.getId(),0));
-		//	TODO send email to user
-		// TODO send email to owner
+		short days=(short) start.until(end, ChronoUnit.DAYS);
+		double fullPrice=place.getPrice()*days;
+		
+		String userContent=String.format("You've successfully made reservation for: %s, place type - %s, Address: Country - %s, City - %s, Street - %s, StreetNumber - %d , for %d days and it will cost you %.2f. The owner of the place have 7 days from now to reject your reservation! You can check in your \"Visited places\" section, if he rejects your reservation the place will no longer be there. If the place is there after the 7 days period that means your reservation is completed!", place.getName(),place.getPlaceTypeName(),place.getCountry(),place.getCity(),place.getStreet(),place.getStreetNumber(),days,fullPrice);
+		MailSender.sendEmail(user.getEmail(), userContent);
+		String ownerContent=String.format("%s %s with e-mail: %s, has made a reservation for: %s, place type - %s, Address: Country - %s, City - %s, Street - %s, StreetNumber - %d , for %d days and it will cost %.2f. If you want to reject the reservation you have to log in our site and go to \"Reservations\" section and then click on the \"Reject  reservation\" button on the reservation! You have 7 days to reject it or it will be automaticly completed", user.getFirstName(),user.getLastName(),user.getEmail(),place.getName(),place.getPlaceTypeName(),place.getCountry(),place.getCity(),place.getStreet(),place.getStreetNumber(),days,fullPrice);
+		MailSender.sendEmail(userDAO.userFromId(place.getOwnerId()).getEmail(), ownerContent);
+		
 		return "home";
 		} catch (Exception e) {
 			e.printStackTrace();
